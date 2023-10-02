ba3820633b447bf0cbd78c63b131bcf5c65fb1a2
GhostEsso <lilipitaham@gmail.com>
2023-10-02 11:59:36 +0000

    student class created

---
+++ student.rb
@@ -0,0 +1,11 @@
+class Student < Person
+    def initialize(name = "Unknown", age = 0, parent_permission = true, classroom)
+      super(name, age, parent_permission)
+      @classroom = classroom
+    end
+  
+    def play_hooky
+      "¯\\(ツ)/¯"
+    end
+  end
+  
