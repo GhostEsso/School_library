2866633ab2b78a7a9ca5d5cd3899c1343d8afdfc
GhostEsso <lilipitaham@gmail.com>
2023-10-02 12:04:32 +0000

    teacher class added

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+class Teacher < Person
+    def initialize(name = "Unknown", age = 0, specialization)
+      super(id, name, age, parent_permission: true)
+      @specialization = specialization
+    end
+  
+    def can_use_services?
+      true
+    end
+  end
+  
