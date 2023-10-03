f4334528c4d309dd7ee1324672d9e3a00821a717
GhostEsso <lilipitaham@gmail.com>
2023-10-03 11:56:18 +0000

    decorator file added

---
+++ decorator.rb
@@ -0,0 +1,12 @@
+require_relative 'Nameable'
+
+class Decorator < Nameable
+    def initialize(nameable)
+        super()
+        @nameable = nameable
+    end
+
+    def correct_name
+        @nameable.correct_name
+    end
+end
