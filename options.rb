087352c6de6460e0d9d08e89446469064f692d52
Newton Ombese <newtonombese1@gmail.com>
2023-10-09 17:15:43 +0300

    Implement a new file options.rb to help me the code be more DRY

---
+++ options.rb
@@ -0,0 +1,6 @@
+def show_options
+  puts "\nPlease choose and option by entering a number:
+1 - List all books\n2 - List all people\n3 - Create a person
+4 - Create a book\n5 - Create a rental
+6 - List all rentals for a given person id\n7 - Exit"
+end
