c544573fe8fe5c04cd9a1df8ecf0769ee14d2658
Newton Ombese <newtonombese1@gmail.com>
2023-10-09 17:16:04 +0300

    Add a require_relative path of the options.rb file

--- main.rb
+++ main.rb
@@ -1,11 +1,5 @@
 require './app'
-
-def show_options
-  puts "\nPlease choose and option by entering a number:
-1 - List all books\n2 - List all people\n3 - Create a person
-4 - Create a book\n5 - Create a rental
-6 - List all rentals for a given person id\n7 - Exit"
-end
+require_relative './options.rb'
 
 def prompt
   puts 'Welcome to School Library App!'
