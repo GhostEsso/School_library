f441f1e2cd0544bad7b5c24145153c95b19a2c96
Newton Ombese <newtonombese1@gmail.com>
2023-10-12 15:12:18 +0300

    FInal fix

--- app.rb
+++ app.rb
@@ -4,12 +4,9 @@ require './teacher'
 require './book'
 require './rental'
 require_relative 'data/data_manager'
-
 class App
   include JsonHelper
-
   attr_accessor :books, :people, :rentals
-
   def initialize
     @books = []
     @rentals = []
@@ -19,7 +16,6 @@ class App
     # load data used here as Callback func
     load_data
   end
-
   # load Data
   def load_data
     @data_manager.load_data
@@ -27,7 +23,6 @@ class App
     @people = @data_manager.people
     @rentals = @data_manager.rentals
   end
-
   # save data
   def save_data
     @data_manager.save_books
@@ -35,15 +30,12 @@ class App
     @data_manager.save_people
     puts 'Data saved!'
   end
-
   def list_books
     @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
   end
-
   def list_books_with_index
     @books.each_with_index { |book, i| puts "#{i}) Title: \"#{book.title}\", Author: #{book.author}" }
   end
-
   def list_people
     @people.each do |person|
       if person.is_a?(Student)
@@ -55,18 +47,14 @@ class App
       end
     end
   end
-  
-
   def list_people_with_index
     @people.each_with_index do |person, i|
       puts "#{i}) [#{person.class.name}] Name: \"#{person.name}\", ID: #{person.id}, Age: #{person.age}"
     end
   end
-
   def create_person
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     choice = gets.chomp.to_i
-
     case choice
     when 1
       create_student
@@ -76,36 +64,30 @@ class App
       puts "Error: Invalid choice (#{choice})"
     end
   end
-
   def create_student
     student = input_student_details
     @data_manager.people.push(student) if student
     @data_manager.save_data # Sauvegarde après l'ajout de l'étudiant
   end
-
   def create_teacher
     teacher = input_teacher_details
     @data_manager.people.push(teacher) if teacher
     @data_manager.save_data # Sauvegarde après l'ajout de l'enseignant
   end
-
   def create_book
     book = input_book_details
     @data_manager.books.push(book) if book
     @data_manager.save_data # Sauvegarde après l'ajout du livre
   end
-
   def create_rental
     rental = input_rental_details
     @data_manager.rentals.push(rental) if rental
     @data_manager.save_data # Sauvegarde après l'ajout de la location
   end
-
   def list_rentals
     print 'ID of person: '
     id = gets.chomp.to_i
     selected_rentals = @rentals.select { |rental| rental.person.id == id }
-
     if selected_rentals.empty?
       puts "Person with id=#{id} doesn't exist"
     else
@@ -115,9 +97,7 @@ class App
       end
     end
   end
-
   private
-
   def input_student_details
     print 'Age: '
     age = gets.chomp.to_i
@@ -125,17 +105,16 @@ class App
     name = gets.chomp.to_s
     print 'Has parent permission? [Y/N]: '
     parent_permission = gets.chomp.strip.downcase
-
+    # Correction ici : utiliser le bon paramètre pour la création de Student
     if %w[yes y].include?(parent_permission)
-      Student.new('Unknown', age, name, parent_permission: true)
+      Student.new(age, name, true)
     elsif %w[no n].include?(parent_permission)
-      Student.new('Unknown', age, name, parent_permission: false)
+      Student.new(age, name, false)
     else
       puts "Error: Invalid parent permission value (#{parent_permission})"
       nil
     end
   end
-
   def input_teacher_details
     print 'Age: '
     age = gets.chomp.to_i
@@ -145,7 +124,6 @@ class App
     specialization = gets.chomp.to_s
     Teacher.new(specialization, age, name)
   end
-
   def input_book_details
     print 'Title: '
     title = gets.chomp.to_s
@@ -153,47 +131,36 @@ class App
     author = gets.chomp.to_s
     Book.new(title, author)
   end
-
   def input_rental_details
     book_index = select_book_index
-
     if book_index.nil?
       puts "Can not add a record. Book #{book_index} doesn't exist"
       return nil
     end
-
     person_index = select_person_index
-
     if person_index.nil?
       puts "Can not add a record. Person #{person_index} doesn't exist"
       return nil
     end
-
     print 'Date: '
     date = gets.chomp.to_s
     book = @books[book_index]
     person = @people[person_index]
     Rental.new(date, book, person)
   end
-
   def select_book_index
     puts 'Select a book from the following list by number'
     list_books_with_index
     book_index = gets.chomp.to_i
-
     (0...@books.length).include?(book_index) ? book_index : nil
   end
-
   def select_person_index
     puts "\nSelect a person from the following list by number (not id)"
     list_people_with_index
     person_index = gets.chomp.to_i
-
     (0...@people.length).include?(person_index) ? person_index : nil
   end
-
   public
-
   def run
     load_data
     prompt
--- data/data_manager.rb
+++ data/data_manager.rb
@@ -1,21 +1,17 @@
 require 'json'
-
 class DataManager
   attr_accessor :books, :rentals, :people
-
   def initialize
     @books = []
     @rentals = []
     @people = []
   end
-
   # Load Data
   def load_data
     load_books
     load_rentals
     load_people
   end
-
   # Save Data
   def save_data
     save_books
@@ -25,9 +21,7 @@ class DataManager
   rescue StandardError => e
     puts "Error Saving Data: #{e.message}"
   end
-
   private
-
   def save_books
     # Use File.open with a block to automatically close the file after writing
     File.open('./data/books.json', 'w') do |file|
@@ -35,18 +29,14 @@ class DataManager
       file.puts JSON.generate(book_data)
     end
   end
-
   def load_books
     return unless File.exist?('./data/books.json')
-
     json_str = File.read('./data/books.json')
     book_data = JSON.parse(json_str)
-
     @books = book_data.map do |data|
       Book.new(data['title'], data['author'])
     end
   end
-
   def save_rentals
     rentals_data = @rentals.map do |rental|
       {
@@ -55,36 +45,26 @@ class DataManager
         'person' => { 'type' => rental.person.class.name, 'age' => rental.person.age, 'name' => rental.person.name }
       }
     end
-
     File.open('./data/rentals.json', 'w') do |file|
       file.puts JSON.generate(rentals_data)
     end
   end
-
   def load_rentals
     return unless File.exist?('./data/rentals.json')
-
     json_str = File.read('./data/rentals.json')
     rentals_data = JSON.parse(json_str)
-
     @rentals = rentals_data.map do |data|
       book_data = data['book']
       person_data = data['person']
       book = Book.new(book_data['title'], book_data['author'])
       person_class = Object.const_get(person_data['type'])
       person = person_class.new(person_data['age'], person_data['name'])
+      # Assuming 'date' is a valid key in your JSON data
       Rental.new(data['date'], book, person)
     end
   end
-
-  # def create_person(type, age, name)
-  #   person = Object.const_get(type).new(age, name)
-  #   @people.push(person)
-  #   save_people
-  # end
-
   def save_people
-    File.open('data/people.json', 'w') do |file|
+    File.open('./data/people.json', 'w') do |file|
       people_data = @people.map do |person|
         if person.is_a?(Student)
           { "type" => "Student", "id" => person.id, "name" => person.name, "age" => person.age, "parent_permission" => person.parent_permission }
@@ -94,27 +74,23 @@ class DataManager
           # Handle other types if necessary
         end
       end
-  
       file.puts JSON.generate(people_data)
     end
   end
-  
   def load_people
-    return unless File.exist?('data/people.json')
-  
+    return unless File.exist?('./data/people.json')
     @people = []
-    json_str = File.read('data/people.json')
+    json_str = File.read('./data/people.json')
     begin
       people_data = JSON.parse(json_str)
     rescue JSON::ParserError => e
-      puts "Error parsing people.json: #{e.message}"
+      # Handle the JSON parsing error if necessary
       return
     end
-  
     people_data.each do |data|
       if data['type'] == 'Student'
-        parent_permission = data['parent_permission'] && data['parent_permission']['parent_permission']
-        person = Student.new(data['id'].to_i, data['name'], data['age'].to_i, parent_permission)
+        parent_permission = data['parent_permission'] == "true"
+        person = Student.new(data['age'], data['name'], parent_permission)
       elsif data['type'] == 'Teacher'
         person = Teacher.new(data['specialization'], data['age'].to_i, data['name'])
       else
@@ -122,5 +98,5 @@ class DataManager
       end
       @people.push(person)
     end
-  end  
+  end
 end
