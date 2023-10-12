655e75a0ead8e365c7fb914aef3b81332a718795
Newton Ombese <newtonombese1@gmail.com>
2023-10-12 15:27:21 +0300

    Fix rubocop issues

--- student.rb
+++ student.rb
@@ -2,15 +2,18 @@ require './person'
 class Student < Person
   attr_reader :classroom
   attr_accessor :parent_permission
-  def initialize(age, name = 'Unknown', parent_permission)
+
+  def initialize(age, parent_permission, name = 'Unknown')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
     @parent_permission = parent_permission
   end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
